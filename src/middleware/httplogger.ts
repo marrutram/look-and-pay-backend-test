2867445ebc5c763077bce5849013d186696bd7f0
ecaminero <caminero.ediwn@gmail.com>
2019-05-22 12:01:43 -0400

    Adding http logs

--- src/middleware/httplogger.ts
+++ src/middleware/httplogger.ts
@@ -2,5 +2,8 @@ import { logger } from '../logger';
 import { get, indexOf } from 'lodash';
 
 export const httplogger = async (resolve, root, args, context, info) => {
-  logger.log({level: 'info', message: 'HTTP - '});
+  logger.log({
+    level: 'info', 
+    message: `HTTP - operation: ${info.operation.operation}: ${info.fieldName}`, 
+    additional: args });
 };
